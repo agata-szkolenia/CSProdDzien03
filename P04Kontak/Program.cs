d632b831b6e8b91b4f3b8c03eace12f954615f11
agata-szkolenia <a.strojnowska@g.fiok.pl>
2022-07-01 16:54:42 +0200

    dodatkowe zadania

--- P04Kontak/Program.cs
+++ P04Kontak/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace P04Kontak
+namespace P04Kontakt
 {
     internal class Program
     {
@@ -12,6 +12,41 @@ namespace P04Kontak
         {
 
             // a.strojnowska@alx.pl
+
+            // zadania na macierze
+
+            // 8x8 (a najlepiej jako parametr do funkcji)
+
+            // 00000000
+            // 11111111
+            // 22222222
+            // ...
+
+            // 01234567
+            // 01234567
+            // ...
+
+            // 01234567
+            // 12345670
+            // 23456701
+            // ..
+
+            // 00000000
+            // 11111111
+            // 00000000
+            // 11111111
+
+            // 01010101
+            // 10101010
+            // 01010101
+            // 10101010
+
+            // Zadanie na klasy
+            // konto bankowe
+            // - saldo (chronione przed zmianą z zewnątrz)
+            // - można wpłacać (dowolne dodatnie kwoty)
+            // - można wypłacać (dodanie kwoty nie przekraczające salda)
+
 
         }
     }
